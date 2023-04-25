e58052077b98b41bf6cb73880a923316cfb9a448
Anas Mohamed <aamohamedo@icloud.com>
2023-04-25 15:54:28 +0100

    Question Card function updated

--- memory-master/src/Components/QuestionCard/QuestionCard.js
+++ memory-master/src/Components/QuestionCard/QuestionCard.js
@@ -1,9 +1,21 @@
-
-
-
-function QuestionCard () {
-    return (<div className="Question-Card-div"></div>)
+function QuestionCard() {
+  return (
+    <div className="Question-Card-Div">
+      <div className="Question-Div">
+        <p className="Question-P">Question:</p>
+        <div className="Subject-And-Answer-Button-Div">
+          <p className="Subject">Subject</p>
+          <button className="Answer-Button">Answer</button>
+        </div>
+      </div>
+      <div className="Answer-Div">
+        <p className="Answer-P">Answer:</p>
+        <p className="How-Did-You-Do">How did you do?</p>
+      </div>
+      <button className="Previous-Card-Button">Previous Card</button>
+      <button className="Next-Card-Button">Next Card</button>
+    </div>
+  );
 }
 
-export default QuestionCard
-
+export default QuestionCard;
