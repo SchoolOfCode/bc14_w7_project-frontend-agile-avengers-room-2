6d941dcdffab40273735c28aede719b8cacd0acf
Jakir9 <jashraf98@hotmail.co.uk>
2023-04-25 16:34:15 +0100

    added all needed elements/components on pages

--- memory-master/src/Components/QuestionCard/QuestionCard.js
+++ memory-master/src/Components/QuestionCard/QuestionCard.js
@@ -2,15 +2,17 @@ function QuestionCard() {
   return (
     <div className="Question-Card-Div">
       <div className="Question-Div">
-        <p className="Question-P">Question:</p>
+        <p className="Question-P">Question: What is 5 * 5? </p>
+        <button className="Delete-Button"> Delete Question</button>
         <div className="Subject-And-Answer-Button-Div">
           <p className="Subject">Subject</p>
           <button className="Answer-Button">Reveal Answer</button>
         </div>
       </div>
       <div className="Answer-Div">
-        <p className="Answer-P">Answer:</p>
+        <p className="Answer-P">Answer: 25</p>
         <p className="How-Did-You-Do">How did you do?</p>
+        {/* ToDO: add ratings - thumbs up/thumbs down */}
       </div>
       <button className="Previous-Card-Button">Previous Card</button>
       <button className="Next-Card-Button">Next Card</button>
